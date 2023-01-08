804678223beff5e011d0414388682bb749926d0b
박현태 <hyuntae.park1030@navercorp.com>
2023-01-08 22:28:43 +0900

    init commit

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+    private static Map<Long, Member> store = new HashMap<>(); // 실무에서는 동시성 문제 때문에 concurrent hashmap을 사용
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
